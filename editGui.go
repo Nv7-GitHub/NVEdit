0395a5e8c3e27cd76f2e34694927cf66b82854bf
Nv7 <junk4Nv7@gmail.com>
2021-01-23 15:02:20 -0800

    Resizing

--- editGui.go
+++ editGui.go
@@ -8,6 +8,7 @@ var oldX int = -1
 var oldY int = -1
 var origX int
 var origY int
+var num int
 
 func renderLayers() {
 	for i, layer := range layers {
@@ -33,6 +34,9 @@ func renderLayers() {
 			case 0:
 				translateTool()
 				break
+			case 1:
+				resizeTool()
+				break
 			default:
 				break
 			}
@@ -40,6 +44,16 @@ func renderLayers() {
 	} else {
 		oldX = -1
 		oldY = -1
+		num = -1
+	}
+	if len(layers) > 0 {
+		switch menuDat[1].Active {
+		case 1:
+			resizeTool()
+			break
+		default:
+			break
+		}
 	}
 }
 
@@ -59,6 +73,7 @@ func processSelected() {
 		selected = newSelected
 		oldX = -1
 		oldY = -1
+		num = -1
 	}
 }
 
@@ -71,4 +86,60 @@ func translateTool() {
 	}
 	oldX = x
 	oldY = y
+}
+
+func resizeTool() {
+	width := int(float64(layers[selected].im.Width) * layers[selected].ScaleX)
+	height := int(float64(layers[selected].im.Height) * layers[selected].ScaleY)
+	r.DrawRectangle(layers[selected].X-5, layers[selected].Y-5, 10, 10, r.GopherBlue)
+	r.DrawRectangle(layers[selected].X-5+width, layers[selected].Y-5, 10, 10, r.GopherBlue)
+	r.DrawRectangle(layers[selected].X-5+width, layers[selected].Y-5+height, 10, 10, r.GopherBlue)
+	r.DrawRectangle(layers[selected].X-5, layers[selected].Y-5+height, 10, 10, r.GopherBlue)
+
+	if r.IsMouseButtonDown(r.MouseLeftButton) {
+		x, y := r.GetMouseX(), r.GetMouseY()
+
+		if x > layers[selected].X-5 && x < layers[selected].X+5 {
+			if y > layers[selected].Y-5 && y < layers[selected].Y+5 {
+				num = 0
+			} else if y > layers[selected].Y-5+height && y < layers[selected].Y+5+height {
+				num = 1
+			}
+		} else if x > layers[selected].X-5+width && x < layers[selected].X+5+width {
+			if y > layers[selected].Y-5 && y < layers[selected].Y+5 {
+				num = 2
+			} else if y > layers[selected].Y-5+height && y < layers[selected].Y+5+height {
+				num = 3
+			}
+		}
+
+		if oldX != -1 && oldY != -1 && num != -1 {
+			var xop float64 = 1
+			var yop float64 = 1
+			if num == 0 {
+				xop = -1
+				yop = -1
+			} else if num == 1 {
+				xop = -1
+			} else if num == 2 {
+				yop = -1
+			}
+			layers[selected].ScaleX += float64(x-oldX) / float64(layers[selected].im.Width) * xop
+			layers[selected].ScaleY += float64(y-oldY) / float64(layers[selected].im.Height) * yop
+			imCache := layers[selected].im.Copy()
+			imCache.Resize(int(layers[selected].ScaleX*float64(layers[selected].im.Width)), int(layers[selected].ScaleY*float64(layers[selected].im.Height)))
+			layers[selected].tex.Unload()
+			layers[selected].tex = r.LoadTextureFromImage(imCache)
+			if num == 0 {
+				layers[selected].X += x - oldX
+				layers[selected].Y += y - oldY
+			} else if num == 1 {
+				layers[selected].X += x - oldX
+			} else if num == 2 {
+				layers[selected].Y += y - oldY
+			}
+		}
+		oldX = x
+		oldY = y
+	}
 }
