8f97c8c26d1cbea7d64e47b460d597144d7cb70b
Nv7 <junk4Nv7@gmail.com>
2021-01-23 23:13:51 -0800

    Linux didn't work, native linux script

---
+++ build-linux.sh
@@ -0,0 +1,3 @@
+# Fyne-cross failed with linux, so this is a script for linux users to compile it themselves
+# linux output: NVEdit.tar.gz
+fyne package -os linux -icon Icon.png -appID com.nvcode.nvedit
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# Mac Output: fyne-cross/dist/darwin-amd64/NVEdit.app
+# Windows Output: fyne-cross/dist/windows-amd64/NVEdit.exe.zip
 fyne-cross darwin -app-id com.nvcode.nvedit
 fyne-cross windows -app-id com.nvcode.nvedit
-fyne-cross linux -app-id com.nvcode.nvedit
