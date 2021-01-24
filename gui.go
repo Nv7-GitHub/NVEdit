98de971bff3e3631dbbc1b6c55c72293195b6b29
Nv7 <junk4Nv7@gmail.com>
2021-01-23 21:16:28 -0800

    Hooked up some keybinds

--- gui.go
+++ gui.go
@@ -63,4 +63,30 @@ func menu() {
 			menuDat[i].Active = val.Handler(menuDat[i].Active, i)
 		}
 	}
+
+	if r.IsKeyPressed(r.KeyOne) {
+		menuDat[1].Active = 0
+	}
+	if r.IsKeyPressed(r.KeyTwo) {
+		menuDat[1].Active = 1
+	}
+	if r.IsKeyPressed(r.KeyThree) {
+		menuDat[1].Active = 2
+	}
+	if r.IsKeyPressed(r.KeyFour) {
+		menuDat[1].Active = 3
+	}
+	if r.IsKeyPressed(r.KeyBackspace) || r.IsKeyPressed(r.KeyDelete) {
+		copy(layers[selected:], layers[selected+1:])
+		layers[len(layers)-1] = Layer{}
+		layers = layers[:len(layers)-1]
+	}
+	if r.IsKeyPressed(r.KeyI) {
+		filename, err := dialog.File().Filter("Image", "png", "jpg", "jpeg").Load()
+		if err != nil && err.Error() == "Cancelled" {
+			return
+		}
+		handle(err)
+		loadLayer(filename)
+	}
 }
