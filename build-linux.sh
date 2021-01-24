eef84039d908b708e5096f7439b1ee646dc9d6ae
Nv7 <junk4Nv7@gmail.com>
2021-01-24 09:07:56 -0800

    Fix linux build script

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,4 @@
 # Fyne-cross failed with linux, so this is a script for linux users to compile it themselves
 # linux output: NVEdit.tar.gz
+go build -o NVEdit -ldflags="-s -w"
 fyne package -os linux -icon Icon.png -appID com.nvcode.nvedit
